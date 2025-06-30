7a894da8d74b1ad4ada1577f0e09ec691b9c248b
Afaf <khadraouiafaf23@gmail.com>
2025-06-30 04:55:06 +0100

    main page added

---
+++ src/Container/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css';
+import { useScroll } from '../Context/ScrollContext.tsx';
+const Navbar: React.FC = () => {
+  const { scrollTo } = useScroll();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-links">
+          <button onClick={() => scrollTo('home')} className="navbar-link">Home</button>
+          <button onClick={() => scrollTo('support')} className="navbar-link">Support</button>
+          <button onClick={() => scrollTo('Testimonials')} className="navbar-link">Testimonials</button>
+          <button onClick={() => scrollTo('benefits')} className="navbar-link">Benefits</button>
+          <button onClick={() => scrollTo('about')} className="navbar-link">About Us</button>
+        </div>
+
+        <div className="navbar-actions">
+          <button className="navbar-login">Login</button>
+          <button className="navbar-signup">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import LandingPage from '../Components/Landing/LandingPage.tsx';
+import Support from '../Components/support/support.tsx';
+import { useScroll } from '../Context/ScrollContext.tsx';
+import Testimonials from '../Components/Testimonials/Testimonials.tsx';
+import BenefitsSection from '../Components/BenefitSection/BenefitSection.tsx';
+const Home: React.FC = () => {
+  const { refs } = useScroll();
+
+  return (
+    <>
+      <div ref={refs.home}><LandingPage /></div>
+      <div ref={refs.support}><Support /></div>
+      <div ref={refs.Testimonials}><Testimonials /></div>
+      <div ref={refs.BenefitsSection}><BenefitsSection /></div>
+      {/* Add other sections later */}
+    </>
+  );
+};
+
+export default Home;
